af89e4fadd90a52732fdaf25b17dc2b79cfdf1b8
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-08-29 22:42:18 -0300

    refatorando

--- src/sql-parse-context.hpp
+++ src/sql-parse-context.hpp
@@ -3,37 +3,7 @@
 #include <string>
 #include <cstdint>
 
-enum Token_Type {
-  NONE, // não inicializado
-  SELECT,
-  FROM,
-  ASTERISK,
-  COMMA,
-  // non-terminal
-  IDENT
-};
-
-std::string get_description(Token_Type &token_type);
-
-
-struct Ident_Token
-{
-  std::string ident;
-  bool quoted;
-  bool dotted;
-  
-  std::string to_string();
-};
-
-
-struct Token
-{
-  Token_Type type;
-  void* data;
-
-  std::string to_string();
-};
-
+#include "./token.hpp"
 
 
 constexpr int32_t END_OF_SOURCE = -9999;
