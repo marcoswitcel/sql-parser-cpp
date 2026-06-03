42d6504d3834aa29efe109d983459cb18c48fb2f
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-06-02 22:13:02 -0300

    WIP: novo  protótipo agregador

--- src/aggregator.cpp
+++ src/aggregator.cpp
@@ -5,17 +5,55 @@
 #include "./aggregator.hpp"
 
 
-void Aggregator::add_subgrouping(std::unique_ptr<Aggregator> aggregator)
+void Value_Aggregator::aggregate(CSV_Data_Row* row)
 {
-  this->type = Aggregator_Type::Subgrouping;
-  this->next_aggregator = std::move(aggregator);
-};
+  auto result = this->field_resolver->resolve(*row);
+        
+  auto result_set = this->ordered_data.lookup(result);
 
-void Aggregator::aggregate(CSV_Data_Row* row)
-{
-  if (this->type == Aggregator_Type::Subgrouping)
+  // se tem adiciona
+  if (result_set)
   {
-    this->next_aggregator->aggregate(row);
-    return;
+    result_set->push_back(row);
   }
+  else
+  {
+    // se não tem cria e adiciona e vincula
+    std::vector<CSV_Data_Row*> new_set;
+    new_set.push_back(row);
+
+    this->ordered_data.put(result, new_set);
+  }
+}
+
+std::unique_ptr<Aggregator> Value_Aggregator::clone()
+{
+  return std::make_unique<Value_Aggregator>(*this);
+}
+
+void Subgrouping_Aggregator::aggregate(CSV_Data_Row* row)
+{
+  auto result = this->field_resolver->resolve(*row);
+        
+  auto result_set = this->ordered_data.lookup(result);
+
+  // se tem adiciona
+  if (result_set)
+  {
+    result_set->aggregate(row);
+  }
+  else
+  {
+    // @todo João, aqui precisa ter uma factorie?
+    // se não tem cria e adiciona e vincula
+    // std::vector<CSV_Data_Row*> new_set;
+    // new_set.push_back(row);
+
+    //this->ordered_data.put(result, new_set);
+  }
+}
+
+std::unique_ptr<Aggregator> Subgrouping_Aggregator::clone()
+{
+  return std::make_unique<Subgrouping_Aggregator>(*this);
 }
--- src/aggregator.hpp
+++ src/aggregator.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "./ordered_map.hpp"
+#include "./resolver.hpp"
 
 // Dependências
 #include "../lib/csv/src/csv.hpp"
@@ -13,15 +14,49 @@ enum class Aggregator_Type { Values, Subgrouping };
 
 struct Aggregator
 {
-  Aggregator_Type type = Aggregator_Type::Values;
-
-  std::unique_ptr<Aggregator> next_aggregator;
-  std::unique_ptr<Ordered_Map<std::string, std::vector<CSV_Data_Row*>>> data;
+  Aggregator_Type type;
+  std::unique_ptr<Field_By_Name_Resolver> field_resolver;
 
   ~Aggregator() = default;
 
-  // métodos de setup
-  void add_subgrouping(std::unique_ptr<Aggregator> aggregator);
+  virtual void aggregate(CSV_Data_Row* row) = 0;
+
+  virtual std::unique_ptr<Aggregator> clone() = 0;
+};
+
+struct Value_Aggregator: Aggregator
+{
+  Aggregator_Type type = Aggregator_Type::Values;
+  /**
+   * @brief Não é responsável pelos ponteiros
+   */
+  Ordered_Map<std::string, std::vector<CSV_Data_Row*>> ordered_data;
+  
+
+  Value_Aggregator(std::unique_ptr<Field_By_Name_Resolver> &field_resolver)
+  {
+    this->field_resolver = std::move(field_resolver);
+  };
 
   void aggregate(CSV_Data_Row* row);
+  
+  std::unique_ptr<Aggregator> clone();
+};
+
+struct Subgrouping_Aggregator: Aggregator
+{
+  Aggregator_Type type = Aggregator_Type::Subgrouping;
+  /**
+   * @brief Não é responsável pelos ponteiros
+   */
+  Ordered_Map<std::string, Aggregator> ordered_data;
+
+  Subgrouping_Aggregator(std::unique_ptr<Field_By_Name_Resolver> &field_resolver)
+  {
+    this->field_resolver = std::move(field_resolver);
+  };
+
+  void aggregate(CSV_Data_Row* row);
+  
+  std::unique_ptr<Aggregator> clone();
 };
